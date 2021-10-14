c92e77a7784f33add1f492bdb3cac54d71b2c92c
Brian Cho <briancho@Brians-MacBook-Air.local>
2021-10-14 09:47:54 -0400

    added Game.java

---
+++ src/main/java/usecases/Game.java
@@ -0,0 +1,17 @@
+package usecases;
+import java.util.ArrayList;
+
+import entities.Deck;
+import entities.Card;
+
+public abstract class Game {
+    private ArrayList<Player> players;
+    private deck Deck;
+    private Player currPlayer;
+
+    public Game(){
+        
+    }
+
+
+}
