d1f09422537ca90c8992eda648decfb5cfef65ff
azamat-khamidov <azamat.khamidov@mail.utoronto.ca>
2021-11-04 01:02:05 -0400

    Overloaded AddToHand by creating another method that takes an ArrayList of Cards as input and adds it to the Player's hand. Created another removeFromHand method that removes and returns an ArrayList of Cards with the given rank. Both implemented methods to be used in GoFish

--- src/main/java/usecases/Player.java
+++ src/main/java/usecases/Player.java
@@ -3,6 +3,9 @@ package usecases;
 import entities.Card;
 import entities.Hand;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /***
  * Player class represents a player that plays implementations of Game. Each player contains a hand,
  * which is the cards the player holds at a given moment.
@@ -31,6 +34,14 @@ public class Player {
     }
 
     /**
+     * adds the inputted list of cards to the hand of this player.
+     * @param cards an ArrayList of cards to add to the hand.
+     */
+    public void addToHand(ArrayList<Card> cards) {
+        this.hand.addCard(cards);
+    }
+
+    /**
      * removes a card that is present in the hand.
      * assumes that the card to be removed is in hand
      *
@@ -38,6 +49,15 @@ public class Player {
      */
     public void removeFromHand(Card card) {
         this.hand.removeCard(card);
+    }
+
+    /**
+     * remove and return a list of cards with the given rank from this player's hand.
+     * @param rank the rank of the cards.
+     * @return an ArrayList of cards with the given rank that have been removed from this player's hand.
+     */
+    public ArrayList<Card> removeFromHand(String rank) {
+        return this.hand.removeCard(rank);
     }
 
     public Hand getHand() {
