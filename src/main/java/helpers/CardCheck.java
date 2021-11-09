aee17de0fe4e59adaefcf058f073213442792edf
Luke-Avveduto <Luke.Avveduto@mail.utoronto.ca>
2021-11-08 19:26:51 -0500

    Added documentation to the CardCheck class.
    This documentation includes all required docstrings as well as some inline comments explaining how the built-in Pattern class works.
    
    I also fixed the regex so it no longer accepts rank values of 0 and 1.

--- src/main/java/helpers/CardCheck.java
+++ src/main/java/helpers/CardCheck.java
@@ -4,12 +4,22 @@ import java.util.regex.Pattern;
 
 /**
  * CardCheck defines methods for checking the validity of a card String.
+ *
+ * This done by compiling a regular expression using the built-in Pattern class.
+ *
+ * The string representations of cards are then considered valid if they are an element of the produced language.
  */
 public class CardCheck {
 
-    private static final Pattern CARD_PATTERN = Pattern.compile("(?i)^(10|\\d|[ajqk])([cdhs])$");
-
+    //Produces a Pattern to check card strings against. If they follow the Pattern, it is a valid card.
+    private static final Pattern CARD_PATTERN = Pattern.compile("(?i)^(10|[2-9]|[ajqk])([cdhs])$");
+    /**
+     * A method that checks if a given string s is a valid representation of a card as a string.
+     * @param s - The string that is being checked as a valid representation of a card as a string.
+     * @return True if the string is a valid representation of a card as a string, false otherwise.
+     */
     public static boolean checkCard(String s) {
+        //Creates a Matcher object and then asks if the given string s matches the CARD_PATTERN
         return CARD_PATTERN.matcher(s).matches();
     }
 }
