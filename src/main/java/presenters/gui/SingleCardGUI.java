de8a54297fcc90583b7fb7230897b51838f8c5ab
Luke-Avveduto <Luke.Avveduto@mail.utoronto.ca>
2021-11-10 17:39:40 -0500

    Added an implementation of GameTemplate.Output that uses a GUI. The GUI is implemented with Swing. When given a card as string, it creates as window that shows a picture of that.
    This is to be used whenever you want a way of displaying a single card to the user such as the top card in crazy eights.

---
+++ src/main/java/presenters/gui/SingleCardGUI.java
@@ -0,0 +1,62 @@
+package presenters.gui;
+
+import usecases.GameTemplate;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingleCardGUI implements GameTemplate.Output {
+
+    private final JFrame frame = new JFrame();
+
+    private final JPanel panel = new JPanel();
+
+    private final Map<String, String> stringToImage;
+
+    public SingleCardGUI(String card) {
+        final String[] CARDS_STRINGS = {"as", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "10s", "js", "qs", "ks",
+                "ac", "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "10c", "jc", "qc", "kc",
+                "ad", "2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "10d", "jd", "qd", "kd",
+                "ah", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "10h", "jh", "qh", "kh"};
+        final String[] CARD_IMAGES = {
+                "1_spade.png", "2_spade.png", "3_spade.png", "4_spade.png", "5_spade.png", "6_spade.png", "7_spade.png", "8_spade.png", "9_spade.png", "10_spade.png", "jack_spade.png", "queen_spade.png", "king_spade.png",
+                "1_club.png", "2_club.png", "3_club.png", "4_club.png", "5_club.png", "6_club.png", "7_club.png", "8_club.png", "9_club.png", "10_club.png", "jack_club.png", "queen_club.png", "king_club.png",
+                "1_diamond.png", "2_diamond.png", "3_diamond.png", "4_diamond.png", "5_diamond.png", "6_diamond.png", "7_diamond.png", "8_diamond.png", "9_diamond.png", "10_diamond.png", "jack_diamond.png", "queen_diamond.png", "king_diamond.png",
+                "1_heart.png", "2_heart.png", "3_heart.png", "4_heart.png", "5_heart.png", "6_heart.png", "7_heart.png", "8_heart.png", "9_heart.png", "10_heart.png", "jack_heart.png", "queen_heart.png", "king_heart.png"
+        };
+
+        //Creates the map from cards as strings to their images file names
+        this.stringToImage = new HashMap<>();
+        for (int i = 0; i < CARD_IMAGES.length; i++) {
+            this.stringToImage.put(CARDS_STRINGS[i], CARD_IMAGES[i]);
+        }
+
+        this.panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
+
+        this.set_card(card);
+
+        this.update();
+
+    }
+
+    private void set_card(String card) {
+        JLabel icon = new JLabel("src/main/resources/cards/" + this.stringToImage.get(card));
+        this.panel.add(icon);
+        this.update();
+    }
+
+    private void update() {
+        this.frame.add(this.panel, BorderLayout.CENTER);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setTitle("Testing Window");
+        this.frame.pack();
+        this.frame.setVisible(true);
+    }
+
+    @Override
+    public void sendOutput(Object s) {
+        this.set_card(s.toString());
+    }
+}
