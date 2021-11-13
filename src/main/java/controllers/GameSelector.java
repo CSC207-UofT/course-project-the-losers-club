c87c61d54586bd65bd8766b8b7a77dccadd43a7f
teddy <teddyrkolev@gmail.com>
2021-11-13 15:20:43 -0500

    test

--- src/main/java/controllers/GameSelector.java
+++ src/main/java/controllers/GameSelector.java
@@ -131,6 +131,7 @@ public class GameSelector {
                     userManager.addUser(username);
                 } catch (UserManager.UserAlreadyExistsException ignored) {
                 }
+
             }
 
             handleUserSelection(sel, usernames, userManager);
