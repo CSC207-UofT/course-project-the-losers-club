7b82c1aec6b0e356972c5f22dc343d925657ed71
azamat-khamidov <azamat.khamidov@mail.utoronto.ca>
2021-11-13 19:53:53 -0500

    Removed redundant imports

--- src/test/java/usecases/GoFishTest.java
+++ src/test/java/usecases/GoFishTest.java
@@ -1,7 +1,6 @@
 package usecases;
 
 import controllers.GameSelector;
-import entities.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
