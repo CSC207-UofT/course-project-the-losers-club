1b98449dee65d3f7d0402094ca7f0a85a1a451c9
Raymond Li <34190736+raylfli@users.noreply.github.com>
2021-11-14 20:45:22 -0500

    Remove CrazyEight commented legacy code for user management

--- src/main/java/usecases/CrazyEights.java
+++ src/main/java/usecases/CrazyEights.java
@@ -135,23 +135,8 @@ public class CrazyEights extends GameTemplate {
             }
             this.currPlayerIndex = (this.currPlayerIndex + 1) % this.players.length;
         }
-        this.addUserStats(this.currPlayer.getUsername());
 
-//        for (String u : this.usernames) {
-//            if (this.currPlayer.getUsername().equals(u)) {
-//                try {
-//                    this.userManager.addGamesPlayed(u, 1);
-//                } catch (UserManager.UserNotFoundException e) {
-//                    e.printStackTrace();
-//                }
-//            } else {
-//                try {
-//                    this.userManager.addGamesPlayed(u, -1);
-//                } catch (UserManager.UserNotFoundException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
+        this.addUserStats(this.currPlayer.getUsername());
         this.PLAYER_GUI.close(this.currPlayer.getUsername() + " Wins!!!");
         this.SINGLE_CARD_GUI.close();
     }
