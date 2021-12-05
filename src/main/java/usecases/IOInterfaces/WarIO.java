091e6cf7fcba6bd1482aec2c46f993107318ad4c
BinyuanZhu <danielb.zhu@mail.utoronto.ca>
2021-12-04 21:27:28 -0500

    Made the WarIO file

--- src/main/java/usecases/IOInterfaces/WarIO.java
+++ src/main/java/usecases/IOInterfaces/WarIO.java
@@ -5,4 +5,36 @@ package usecases.IOInterfaces;
  * constructor and be used to play War.
  */
 public interface WarIO {
+    /**
+     * This method should change the current user to <>username</> and then informers the user of this change.
+     *
+     * @param username The <>username</> of the next player.
+     */
+    void changePlayer(String username);
+
+    /**
+     * This method should display the new top card to the user.
+     *
+     * @param card a string representation of the card to be displayed.
+     */
+    void showTopCard(String card);
+
+    /**
+     * This method should send a popup to the user containing a <>message</>.
+     *
+     * @param message a string that is to be sent to the user
+     */
+    void sendPopup(String message);
+
+    /**
+     * This method should close the GUI when called.
+     */
+    void close();
+
+    /**
+     * This method should display a <>message</> to the user and then close the GUI.
+     *
+     * @param message a string that is to be sent to the user.
+     */
+    void closeMessage(String message);
 }
